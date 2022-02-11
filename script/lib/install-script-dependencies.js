62f3583c8d26ab7c18e9525caf9100752eaeb80c
Vini Dalvino <37189791+PissMan420@users.noreply.github.com>
2022-02-11 14:20:31 -0500

    Added command line option Added an command line option to use yarn instead of npm.

--- script/lib/install-script-dependencies.js
+++ script/lib/install-script-dependencies.js
@@ -7,11 +7,15 @@ const CONFIG = require('../config');
 // Recognised by '@electron/get', used by the 'electron-mksnapshot' and 'electron-chromedriver' dependencies
 process.env.ELECTRON_CUSTOM_VERSION = CONFIG.appMetadata.electronVersion;
 
-module.exports = function(ci) {
-  console.log('Installing script dependencies');
-  const npmBinName = process.platform === 'win32' ? 'npm.cmd' : 'npm';
+module.exports = function(ci, yarn) {
+  console.log('Installing script dependencies using ' + (yarn ? 'yarn' : 'npm'));
+  let pkgManager;
+  if (yarn)
+    pkgManager = process.platform === 'win32' ? 'yarn.cmd' : 'yarn';
+  else
+    pkgManager = process.platform === 'win32' ? 'npm.cmd' : 'npm';
   childProcess.execFileSync(
-    npmBinName,
+    pkgManager,
     ['--loglevel=error', ci ? 'ci' : 'install'],
     { env: process.env, cwd: CONFIG.scriptRootPath }
   );
